13d64334108f105922619804dbc8c53254ffbd51
Shaik Aseeruddin <84238767+Aseer123@users.noreply.github.com>
2024-10-30 12:40:21 +0530

    Add files via upload

---
+++ BootSecurity-jwt/target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into sec_user(id,user_name,password,roles)
+values(1,'Prasanna','1234','ADMIN');
+insert into sec_user(id,user_name,password,roles)
+values(2,'Padma','5678','USER');
+insert into sec_user(id,user_name,password,roles)
+values(3,'Sarojini','3457','HR');
